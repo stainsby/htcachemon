66f4e95ed35d38250ee5a6cefb6ade32f4d59f50
Sam Stainsby <sam@stainsby.id.au>
2016-06-09 11:09:02 +1000

    added filtering and pagination to UI; added TaffyDB

--- ui/app.js
+++ ui/app.js
@@ -7,34 +7,131 @@
 
   ngApp.constant('moment', window.moment);
   ngApp.constant('filesize', window.filesize);
-  ngApp.constant('taffy', window.TAFFY);
+  ngApp.constant('db', window.TAFFY());
 
 
-  ngApp.factory('Entries', function($log, $resource, config) {
+  ngApp.factory('entries', function($log, $resource, config) {
     var entryPath = config.api.endpoint + '/entries';
     $log.info('API entry path', entryPath);
-    return $resource(entryPath);
+    return $resource(
+      entryPath,
+      {
+        actions: {
+          get: {method:'GET', isArray: false},
+          delete: {method:'DELETE'},
+        }
+      }
+    );
   });
 
 
-  ngApp.controller('MainController', function($log, $resource, $scope, moment, filesize, status, Entries) {
+  ngApp.controller('MainController', function($log, $resource, $scope, $rootScope, config, moment, filesize, status, entries, db) {
+
     $scope.moment = moment;
     $scope.filesize = filesize;
     $scope.loaded = false;
     $scope.status = status;
-    Entries.get(
-      {},
-      function success(entries) {
+    $scope.urlFilter = '';
+
+    function doDatabaseRefresh() {
+      $log.info('starting database refresh');
+      $log.info('fetching cache entry stats');
+      entries.get().$promise.then(function(entryMap) {
+        delete entryMap.$promise;
+        delete entryMap.$resolved;
+        $log.info('got entry stats for', Object.keys(entryMap).length, 'URLs');
+        $log.info('clearing the database');
+        db().remove(true); // clear the database
+        $log.info('inserting new data into the database');
+        angular.forEach(entryMap, function(entries, url) {
+          angular.forEach(entries, function(entry) {
+            entry.url = url;
+            db.insert(entry);
+          });
+        });
+        $log.info('database refresh done');
         $scope.loaded = true;
-        $scope.entries = entries;
-        var urls = Object.keys(entries).length;
-        $log.debug('fetched cache entries OK, unpacking entries for', urls.length, 'URLs');
-        // $.each(urls, functiomn(idx, entries))
-      },
-      function failure(err) {
-        $log.error('ERROR:', err);
-      }
-    );
+        doUrlFilter();
+      });
+    }
+
+    function doUrlFilter() {
+      var filter = $scope.urlFilter.trim();
+      $log.info('applying URL filter:', filter);
+      $scope.loaded = false;
+      var results = (filter ? db( { url: { like: filter } } ) : db()).order('url, expiry');
+      $log.info('found', results.count(), 'results');
+      var entries = [];
+      var currentUrl;
+      var currentEntrySet = [];
+      var url;
+      results.each(function(entry) {
+        url = entry.url;
+        // delete entry.url;
+        if (currentUrl !== undefined && url !== currentUrl) {
+          entries.push({ url: currentUrl, stats: currentEntrySet });
+          currentEntrySet = [];
+        }
+        currentUrl = url;
+        currentEntrySet.push(entry);
+      });
+      entries.push({ url: currentUrl, stats: currentEntrySet }); // don't forget the last set!
+
+      var totalCount = entries.length;
+      var entriesPerPage = $rootScope.search.entriesPerPage;
+      var page = 1;
+      var entryIndexStart = (page - 1)*entriesPerPage;
+      var pageEntryCount = Math.min(totalCount - entryIndexStart, entriesPerPage);
+      var lastPage = Math.ceil(totalCount/entriesPerPage);
+
+      $rootScope.search.entries = entries;
+      $rootScope.search.totalCount = totalCount;
+      $rootScope.search.page = page;
+      $rootScope.search.lastPage = lastPage;
+      // $rootScope.search.entryIndexStart = entryIndexStart;
+      // $rootScope.search.pageEntryCount = pageEntryCount;
+      // $scope.urlEntries = $rootScope.search.entries.slice(entryIndexStart, entryIndexStart + pageEntryCount);
+
+      onPagination();
+
+      $scope.loaded = true;
+
+    }
+
+
+    function onPagination() {
+      var page = $rootScope.search.page;
+      var totalCount = $rootScope.search.totalCount;
+      var entriesPerPage = $rootScope.search.entriesPerPage;
+      var entryIndexStart = (page - 1)*entriesPerPage;
+      var pageEntryCount = Math.min(totalCount - entryIndexStart, entriesPerPage);
+      $rootScope.search.entryIndexStart = entryIndexStart;
+      $rootScope.search.pageEntryCount = pageEntryCount;
+      $scope.urlEntries = $rootScope.search.entries.slice(entryIndexStart, entryIndexStart + pageEntryCount);
+      $rootScope.entryShowState = {};
+    }
+
+
+    function pageForward() {
+      var page = $rootScope.search.page;
+      var lastPage = $rootScope.search.lastPage;
+      $rootScope.search.page = page === lastPage ? page : page + 1;
+      onPagination();
+    }
+
+    function pageBackward() {
+      var page = $rootScope.search.page;
+      $rootScope.search.page = page === 1 ? page : page - 1;
+      onPagination();
+    }
+
+    $scope.doDatabaseRefresh = doDatabaseRefresh;
+    $scope.doUrlFilter = doUrlFilter;
+    $scope.pageForward = pageForward;
+    $scope.pageBackward = pageBackward;
+    
+    doDatabaseRefresh();
+
   });
 
 
@@ -51,9 +148,12 @@
   }]);
 
 
-  ngApp.run(function($rootScope, $log) {
+  ngApp.run(function($rootScope, $log, config) {
     $log.info('htcachemon running');
-    $rootScope.db = null;
+    $rootScope.search = {
+      entriesPerPage: config.search.entriesPerPage
+    };
+    $rootScope.entryShowState = {};
   });
 
 
--- ui/config.js
+++ ui/config.js
@@ -2,14 +2,19 @@
 
   'use strict';
 
-  var ngApp = angular.module('htcachemon');
 
-
-  ngApp.constant('config', {
+  var CONFIG = {
     api: {
       endpoint: "http://127.0.0.1:6453"
+    },
+    search: {
+      entriesPerPage: 4
     }
-  });
+  };
 
+
+  var ngApp = angular.module('htcachemon');
+
+  ngApp.constant('config', CONFIG);
 
 })();
